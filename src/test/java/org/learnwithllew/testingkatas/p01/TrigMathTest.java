4950833874b181d84057b79b0529a4cd707104db
emilybache <emily.bache@praqma.com>
2018-08-21 08:54:16 +0200

    tweaked the starting position a little to make it easier to get started

--- src/test/java/org/learnwithllew/testingkatas/p01/TrigMathTest.java
+++ src/test/java/org/learnwithllew/testingkatas/p01/TrigMathTest.java
@@ -1,24 +1,24 @@
 package org.learnwithllew.testingkatas.p01;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 /*
- * Goal: 100% Test coverage of TrigMath
- * Tools: EclEmma (Test Coverage)
- *        ApprovalTests (verification package)
+ * Goal: 100% Test coverage of TrigMath and GenericMath
  */
 public class TrigMathTest
 {
-  @Test
-  public void test()
-  {
-    fail("Not yet implemented");
-  }
-  private void assertDouble(double expected, double actual)
-  {
-    assertEquals(expected + "", actual + "");
-  }
+    @Test
+    public void testFloor() {
+        assertEquals(1.0, GenericMath.floor(1.2), 0.0001);
+
+    }
+
+    @Test
+    public void testSin() {
+        assertEquals(0, TrigMath.sin(0), 0.0001);
+        assertEquals(1, TrigMath.sin(TrigMath.HALF_PI), 0.0001);
+    }
+
 }
